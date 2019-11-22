bc60a95e6a449f90270be222309b56386dcd0b0f
Patxi Oyaga <oyaga888@gmail.com>
2019-11-22 09:50:29 +0100

    Create 7kyu-Computer problem series #2: Torrent download-Patxi.py
    https://www.codewars.com/kata/5d4c6809089c6e5031f189ed

---
+++ 7kyu-Computer problem series #2: Torrent download-Patxi.py
@@ -0,0 +1,31 @@
+def torrent(files):
+    max = 0
+    time = []
+    for x in range(len(files)):
+          temp = files[x]
+          time_temp = int((temp['size_GB']*1000*8)/temp['speed_Mbps'])
+          time.append( time_temp )
+    time_s = sorted(time)
+    max = time_s[len(time_s)-1]
+    ordered = []
+    count = 0
+    for x in range(len(files)):
+          sub_list = []
+          if count > 0: #Jump as many repeated time_s
+              count -= 1
+          elif ( x < len(files)-1 and time_s[x] == time_s[x+1] ): # Until second-to-last check if time_s elements repeat
+              for y in range(len(files)):
+                  temp = files[y]
+                  if time_s[x] == int((temp['size_GB']*1000*8)/temp['speed_Mbps']):
+                      count +=1
+                      sub_list.append(temp['name'])
+              sub_list.sort()
+              ordered.extend(sub_list)
+          elif ( x == len(files) and time_s[x] != time_s[x-1]): #Last time_s element
+              count -= 1
+          else : # Regular not repeated times
+              for y in range(len(files)):
+                  temp = files[y]
+                  if time_s[x] == int((temp['size_GB']*1000*8)/temp['speed_Mbps']):
+                      ordered.append(temp['name'])
+    return (ordered, max)
