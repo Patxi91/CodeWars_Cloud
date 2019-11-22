babf43c4d27bc2b6b79e7b131908374f56a3a998
Patxi Oyaga <oyaga888@gmail.com>
2019-11-22 17:50:23 +0100

    Update 7kyu-Computer problem series #2: Torrent download-Patxi.py
    Time: 896ms Passed: 403 Failed: 0

--- 7kyu-Computer problem series #2: Torrent download-Patxi.py
+++ 7kyu-Computer problem series #2: Torrent download-Patxi.py
@@ -1,35 +1,40 @@
 def torrent(files):
-    print(len(files), files)
     max = 0
     time = []
     for x in range(len(files)):
           temp = files[x]
-          time_temp = int((temp['size_GB']*1000*8)/temp['speed_Mbps'])
+          time_temp = (temp['size_GB']*1000*8)/temp['speed_Mbps']
           time.append( time_temp )
     time_s = sorted(time)
     max = time_s[len(time_s)-1]
     ordered = []
     count = 0
+    print(len(files), files)
+    print(time_s)
     for x in range(len(files)):
           sub_list = []
-          if count > 0: #Jump as many repeated time_s
+          print(count)
+          if count > 1: #Jump as many repeated time_s
               count -= 1
+              if count == 1:
+                  count = 0
+              continue
           elif ( x < len(files)-1 and time_s[x] == time_s[x+1] ): # Until second-to-last check if time_s elements repeat
               for y in range(len(files)):
                   temp = files[y]
-                  if time_s[x] == int((temp['size_GB']*1000*8)/temp['speed_Mbps']):
+                  if time_s[x] == (temp['size_GB']*1000*8)/temp['speed_Mbps']:
                       count +=1
                       sub_list.append(temp['name'])
               sub_list.sort()
               ordered.extend(sub_list)
-          elif ( x == len(files) and time_s[x] != time_s[x-1]): #Last time_s element
+          elif ( x == len(files)-1 and time_s[x] != time_s[x-1]): #Last time_s element
               for y in range(len(files)):
                   temp = files[y]
-                  if time_s[x] == int((temp['size_GB']*1000*8)/temp['speed_Mbps']):
+                  if time_s[x] == (temp['size_GB']*1000*8)/temp['speed_Mbps']:
                       ordered.append(temp['name'])
           else : # Regular not repeated times
               for y in range(len(files)):
                   temp = files[y]
-                  if time_s[x] == int((temp['size_GB']*1000*8)/temp['speed_Mbps']):
+                  if time_s[x] == (temp['size_GB']*1000*8)/temp['speed_Mbps']:
                       ordered.append(temp['name'])
     return (ordered, max)
