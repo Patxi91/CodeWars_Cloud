4aab9adaa6fc4f14debc98523d70f0119bb1f181
ezafeiriadis <58179207+ezafeiriadis@users.noreply.github.com>
2019-11-25 16:07:15 +0100

    Create 4kyu-Simple Fun #27: Rectangle Rotation-Stathis.py

---
+++ 4kyu-Simple Fun #27: Rectangle Rotation-Stathis.py
@@ -0,0 +1,10 @@
+def rectangle_rotation(a, b):
+      a = a / 2 ** 0.5
+      b = b / 2 ** 0.5
+      a= int(a) + 1
+      b= int(b) + 1
+      sum=0
+      sum= sum + a * b +  (b-1) * (a-1)
+      if sum % 2 == 0 :
+          sum = sum - 1
+      return sum
