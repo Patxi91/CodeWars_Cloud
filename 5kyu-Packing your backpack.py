203b379d159a965d320278afb3b91471786c5628
Patxi91 <oyaga888@gmail.com>
2020-12-27 00:32:02 +0100

    5kyu-Packing your backpack.py

---
+++ 5kyu-Packing your backpack.py
@@ -0,0 +1,17 @@
+def pack_bagpack(scores, weights, capacity):
+    W = capacity
+    wt = weights
+    val = scores
+    n = len(weights)
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+    # Build table K[][] in bottom up manner
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif wt[i - 1] <= w:
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w])
+            else:
+                K[i][w] = K[i - 1][w]
+
+    return K[n][W]
