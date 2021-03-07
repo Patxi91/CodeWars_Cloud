e0e4a52fd09cd1df5c67dd8b3673c4c990d60b94
Patxi91 <oyaga888@gmail.com>
2021-03-07 21:57:14 +0100

    5kyu-Regex Password Validation-Patxi

--- 5kyu-Regex Password Validation-Patxi.py
+++ 5kyu-Regex Password Validation-Patxi.py
@@ -1,1 +1,13 @@
+from re import compile, VERBOSE
+
+regex = compile("""
+^              # begin word
+(?=.*?[a-z])   # at least one lowercase letter
+(?=.*?[A-Z])   # at least one uppercase letter
+(?=.*?[0-9])   # at least one number
+[A-Za-z\d]     # only alphanumeric
+{6,}           # at least 6 characters long
+$              # end word
+""", VERBOSE)
+
 regex = '(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*^\s*\S+\s*$)(?=.*^[a-zA-Z0-9]*$).{6,}$'
