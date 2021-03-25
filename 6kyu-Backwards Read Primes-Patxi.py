4df7ba10c91e010b4f9476f101a54cb87f4df337
Patxi91 <oyaga888@gmail.com>
2021-03-25 16:12:22 +0100

    Refactor

---
+++ 6kyu-Backwards Read Primes-Patxi.py
@@ -0,0 +1,28 @@
+
+def is_prime(n):
+
+    """
+    Assumes that n is a positive natural number
+    """
+    # We know 1 is not a prime number
+    if n == 1:
+        return False
+
+    # We store the number of factors in this variable
+    factors = 0
+    # This will loop from 1 to n
+    for i in range(1, n+1):
+        # Check if `i` divides `n`, if yes then we increment the factors
+        if n % i == 0:
+            factors += 1
+    # If total factors are exactly 2
+    if factors == 2:
+        return True
+    return False
+
+
+# Test
+n = 10
+for x in range(1, n+1):
+    print('%d: %s' % (x, is_prime(x)))
+
