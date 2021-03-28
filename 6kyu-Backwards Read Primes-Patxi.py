e53e64db7815631ac5bc767cd8a3aa8537e25f7b
Patxi91 <oyaga888@gmail.com>
2021-03-29 00:09:45 +0200

    6kyu-Backwards Read Primes-Patxi

--- 6kyu-Backwards Read Primes-Patxi.py
+++ 6kyu-Backwards Read Primes-Patxi.py
@@ -1,29 +1,30 @@
+import math
 
+
+# checking for prime
 def is_prime(n):
-
-    # 1 is not a prime number
-    if n == 1:
+    # checking for up to 1
+    if n <= 1:
         return False
-
-    factors = 0  # Number of factors in this variable
-
-    for i in range(1, n+1):
-        # Check if `i` divides `n`, if yes then we increment the factors
-        if n % i == 0:
-            factors += 1
-    # If total factors are exactly 2
-    if factors == 2:
+    # checking for 2
+    elif n == 2:
         return True
-    return False
+    # checking for evens
+    elif n > 2 and n % 2 == 0:
+        return False
+    else:
+        # iterating loop till square root of n
+        for i in range(3, int(math.sqrt(n)) + 1, 2):
+            # checking for factor
+            if n % i == 0:
+                return False
+
+    return True
 
 
 def backwards_prime(start, stop):
     sol = []
-    for x in range(start, stop+1):
-        if is_prime(x)and x != int(str(x)[::-1]) and is_prime(int(str(x)[::-1])):
+    for x in range(start, stop + 1):
+        if is_prime(x) and x != int(str(x)[::-1]) and is_prime(int(str(x)[::-1])):
             sol.append(x)
     return sol
-
-
-# Test
-brp = backwards_prime(2, 100)  # => [13, 17, 31, 37, 71, 73, 79, 97]
