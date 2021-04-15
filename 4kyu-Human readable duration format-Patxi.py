64fd3893a9583e976eff8c0a9c9474059aa526a2
Patxi91 <oyaga888@gmail.com>
2021-04-15 11:46:21 +0200

    4kyu-Human readable duration format-Patxi

---
+++ 4kyu-Human readable duration format-Patxi.py
@@ -0,0 +1,26 @@
+def replace_last(string, find, replace):
+    reversed = string[::-1]
+    replaced = reversed.replace(find[::-1], replace[::-1], 1)
+    return replaced[::-1]
+
+
+def format_duration(seconds):
+    # your code here
+
+    TIME_DURATION_UNITS = (
+        ('year', 60 * 60 * 24 * 365),
+        ('day', 60 * 60 * 24),
+        ('hour', 60 * 60),
+        ('minute', 60),
+        ('second', 1))
+
+    if seconds == 0:
+        return 'now'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'.format(amount, unit, "" if amount == 1 else "s"))
+    result = ', '.join(parts)
+    result = replace_last(result, ",", " and")
+    return result
