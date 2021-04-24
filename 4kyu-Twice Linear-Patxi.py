80b41a756f70b37f7d3bc680b7f5b98ce9fa846d
Patxi91 <oyaga888@gmail.com>
2021-04-25 01:49:51 +0200

    4kyu-Twice Linear-Patxi

--- 4kyu-Twice Linear-Patxi.py
+++ 4kyu-Twice Linear-Patxi.py
@@ -1,25 +1,21 @@
-
-'''
-Consider a sequence u where u is defined as follows:
-
-The number u(0) = 1 is the first one in u.
-For each x in u, then y = 2 * x + 1 and z = 3 * x + 1 must be in u too.
-There are no other numbers in u.
-Example:
-u = [1, 3, 4, 7, 9, 10, 13, 15, 19, 21, 22, 27, ...]
-
-1 gives 3 and 4, then 3 gives 7 and 10, 4 gives 9 and 13, then 7 gives 15 and 22 and so on...
-
-Task:
-Given parameter n the function dbl_linear (or dblLinear...) returns the element u(n) of the ordered sequence u (ordered with < so there are no duplicates) .
-
-Example:
-dbl_linear(10) should return 22
-
-Note:
-Focus attention on efficiency
-'''
-
-
 def dbl_linear(n):
-	pass
+    list = [1]
+    x = 0
+    y = 0
+
+    while (len(list) <= n):
+        a = 2 * list[x] + 1
+        b = 3 * list[y] + 1
+
+        if a > b:
+            list.append(b)
+            y += 1
+        elif a < b:
+            list.append(a)
+            x += 1
+        else:
+            list.append(a)
+            x += 1
+            y += 1
+
+    return list[n]
