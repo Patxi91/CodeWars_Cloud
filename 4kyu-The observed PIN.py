3793de7769cd06f7651824f8d9248eb38f3db4e3
Patxi91 <oyaga888@gmail.com>
2021-04-27 11:05:34 +0200

    4kyu-The observed PIN

--- 4kyu-The observed PIN.py
+++ 4kyu-The observed PIN.py
@@ -1,45 +1,14 @@
-def combinations_with_replacement(iterable, r):
-    # combinations_with_replacement('ABC', 2) --> AA AB AC BB BC CC
-    pool = tuple(iterable)
-    n = len(pool)
-    if not n and r:
-        return
-    indices = [0] * r
-    yield tuple(pool[i] for i in indices)
-    while True:
-        for i in reversed(range(r)):
-            if indices[i] != n - 1:
-                break
-        else:
-            return
-        indices[i:] = [indices[i] + 1] * (r - i)
-        yield tuple(pool[i] for i in indices)
-
-
-def permutations(iterable, r=None):
-    # permutations('ABCD', 2) --> AB AC AD BA BC BD CA CB CD DA DB DC
-    # permutations(range(3)) --> 012 021 102 120 201 210
-    pool = tuple(iterable)
-    n = len(pool)
-    r = n if r is None else r
-    if r > n:
-        return
-    indices = list(range(n))
-    cycles = list(range(n, n-r, -1))
-    yield tuple(pool[i] for i in indices[:r])
-    while n:
-        for i in reversed(range(r)):
-            cycles[i] -= 1
-            if cycles[i] == 0:
-                indices[i:] = indices[i+1:] + indices[i:i+1]
-                cycles[i] = n - i
-            else:
-                j = cycles[i]
-                indices[i], indices[-j] = indices[-j], indices[i]
-                yield tuple(pool[i] for i in indices[:r])
-                break
-        else:
-            return
+def product(*args, repeat=1):
+    """
+    product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+    product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+    """
+    pools = [tuple(pool) for pool in args] * repeat
+    result = [[]]
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool]
+    for prod in result:
+        yield tuple(prod)
 
 
 def join_tup(tup):
@@ -68,18 +37,18 @@ def get_pins(observed):
     digits = list(dict.fromkeys(digits))  # Remove duplicates
     digits_s = "".join([str(i) for i in digits])
 
-    merged_list = list(set(list(permutations(digits_s, len(observed))) + list(combinations_with_replacement(digits_s, len(observed)))))
+    # Generate all possible combination tuples
+    merged_list = list(product(digits_s, repeat=len(observed)))
+    # Tuples to strings of integers
     for tup in range(len(merged_list)):
         merged_list[tup] = join_tup(merged_list[tup])
-    return sorted(merged_list)
 
+    # Filter not possible answers (keypad layout)
+    merged_list_fil = []
+    for num in merged_list:
+        for d in range(len(observed)):
+            if int(num[d]) not in adj_dig[int(observed[d])]:
+                merged_list_fil.append(num)  # Not possible variations
+                break
 
-observed = '369'
-sol = ["11", "22", "44", "12", "21", "14", "41", "24", "42"]
-my_sol = get_pins(observed)
-
-
-
-
-
-
+    return sorted(set(merged_list).difference(merged_list_fil))
