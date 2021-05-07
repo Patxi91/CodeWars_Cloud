532d077266825c247e9f23bd44bc53190dfbcfd8
Patxi Oyaga <oyaga888@gmail.com>
2021-05-07 11:04:20 +0200

    6kyu-Sum consecutives-Patxi

--- 6kyu-Sum consecutives-Patxi.py
+++ 6kyu-Sum consecutives-Patxi.py
@@ -1,7 +1,13 @@
 
 def sum_consecutives(s):
-    pass
+    p = s[:1]   # final list
+    cur = s[0]  # keep track of last seen value
 
+    for i in s[1:]:
+        if i == cur:
+            p[-1] += i
+        else:
+            p.append(i)
+            cur = i
 
-
-
+    return p
