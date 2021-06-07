bf7605da174d392c8580368443699c6c4008553d
Patxi Oyaga <oyaga888@gmail.com>
2021-06-07 16:42:12 +0200

    3kyu-Path Finder #3 the alpinist-Patxi

--- 3kyu-Path Finder #3 the alpinist-Patxi.py
+++ 3kyu-Path Finder #3 the alpinist-Patxi.py
@@ -11,24 +11,55 @@ def str2mat(s):
 
 def path_finder(grid, start=(0, 0)):
     # BFS Preset
-    wall, clear, goal = "W", ".", "*"
     grid_split = str2mat(grid)
-    grid_split[-1][-1] = '*'
     grid_split_back = [''.join(idx for idx in sub) for sub in grid_split]
     width, height = len(grid_split_back[0]), len(grid_split_back)
     flag = False
+    min_climbs = 10e6
     # Action
     queue = collections.deque([[start]])
     seen = set([start])
     while queue:
         path = queue.popleft()
         x, y = path[-1]
-        if grid_split_back[y][x] == goal:
+        if (x, y) == (height-1, width-1):  # Goal
             flag = True
-            return len(path)-1
+            steps = [int(grid_split[index[0]][index[1]]) for index in path]
+            levels_climbed = sum(steps[1:-1])-steps[0]+steps[-1]
+            if levels_climbed < min_climbs:
+                min_climbs = levels_climbed
+                min_path = path
+
+            seen.discard((x2, y2))
+            x, y = path[-1]
+
         for x2, y2 in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
-            if 0 <= x2 < width and 0 <= y2 < height and grid_split_back[y2][x2] != wall and (x2, y2) not in seen:
+            if 0 <= x2 < width and 0 <= y2 < height and (x2, y2) not in seen:
                 queue.append(path + [(x2, y2)])
                 seen.add((x2, y2))
-    if not flag:
+    if flag:
+        return min_climbs
+    else:
         return False
+
+
+
+c = "\n".join([
+  "......",
+  "......",
+  "......",
+  "......",
+  "......",
+  "......"])
+
+f = "\n".join([
+  "777000",
+  "007000",
+  "007000",
+  "007000",
+  "007000",
+  "007777"])
+
+r_climbs = path_finder(f)
+
+
