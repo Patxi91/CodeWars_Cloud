bd32deaef563599f8ba550b1034ab4ef4dd97e6b
Patxi Oyaga <oyaga888@gmail.com>
2021-06-19 14:56:28 +0200

    6kyu-Simple Fun 132 Number Of Carries-Patxi

--- 6kyu-Simple Fun 132 Number Of Carries-Patxi.py
+++ 6kyu-Simple Fun 132 Number Of Carries-Patxi.py
@@ -1,15 +1,18 @@
 import math
 
 def number_of_carries(a, b):
+    a, b = max(a, b), min(a, b)
+    carry = 0
+    sol = 0
+    while a != 0:
+        a_m = a % 10
+        b_m = b % 10
+        a = math.floor(a/10)
+        b = math.floor(b/10)
 
-    a_m = a % 10
-    b_m = b % 10
-    a_i = math.floor(a/10)
-    b_i = math.floor(b/10)
-
-
-
-a = 1927
-b = 6426
-
-
+        if a_m + b_m + carry >= 10:
+            carry = 1
+            sol += 1
+        else:
+            carry = 0
+    return sol
