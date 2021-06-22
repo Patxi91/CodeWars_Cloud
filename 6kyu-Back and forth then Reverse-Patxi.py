d1586a54f91555edd552aa8d43cafa95a2bbcf15
Patxi Oyaga <oyaga888@gmail.com>
2021-06-22 10:33:44 +0200

    6kyu-Back and forth then Reverse-Patxi

--- 6kyu-Back and forth then Reverse-Patxi.py
+++ 6kyu-Back and forth then Reverse-Patxi.py
@@ -1,58 +1,20 @@
-
-def arrange1(s):
-    t = []
-    sc = s.copy()
-    while sc:
-        try:
-            t.append(sc[0])
-            t.append(sc[-1])
-            sc = [x for x in reversed(sc[1:-1])]
-        except:
-            break
-    return t
-
-def arrange2(s):
-    t = []
-    sc = s.copy()
-    while sc:
-        try:
-            t.append(sc[0])
-            t.append(sc[-1])
-            sc = sc[::-1]
-        except:
-            break
-    return t
-
-import math
-def arrange3(s):
-    t = []
-    sc = [x for x in s]
-    for seq in range(math.ceil(len(s)/2)-1):
-        a, *b, c = sc
-        t.extend((a, c))
-        sc = b[::-1]
-    t.extend(sc)
-    return t
-
-
 def arrange(s):
-    if len(s)<2:
-        return s
-    else:
         t = []
-        a, *b, c = s
-        t.extend((a, c))
-        while len(b) > 1:
-            a, *b, c = b[::-1]
-            t.extend((a, c))
-        t.extend(b)
+        first = 0
+        second = len(s)-1
+        flag = True
+        while first <= second:
+            if first == second:
+                t.append(s[first])
+                break
+            else:
+                if flag:
+                    t.append(s[first])
+                    t.append(s[second])
+                else:
+                    t.append(s[second])
+                    t.append(s[first])
+                first += 1
+                second -= 1
+                flag = not flag
         return t
-
-
-
-
-r = arrange([4, 3, 2])
-
-
-
-
