00173019f72a5dba1563ad00e26234007c7f5ccb
Patxi Oyaga <oyaga888@gmail.com>
2021-06-26 23:05:50 +0200

    4kyu-All Balanced Parantheses-Patxi

---
+++ 4kyu-Balanced parentheses string-Patxi.py
@@ -0,0 +1,25 @@
+def generateParentheses(openBr, closeBr, n, s=[]):
+    global arr
+    if closeBr == n:
+        arr.append(''.join(s))
+        return
+
+    if closeBr < openBr:
+        s.append(')')
+        generateParentheses(openBr, closeBr + 1, n, s)
+        s.pop()
+    if openBr < n:
+        s.append('(')
+        generateParentheses(openBr + 1, closeBr, n, s)
+        s.pop()
+    return
+
+
+def balanced_parens(n, k):
+    global arr
+    arr = []
+    generateParentheses(0, 0, n)
+    return arr[-(k+1)] if 0<=k<len(arr) else None
+
+
+
