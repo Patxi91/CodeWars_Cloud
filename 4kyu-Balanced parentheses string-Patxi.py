1220b3a16aefc1e32377b58b89914a445ea89ff3
Patxi Oyaga <oyaga888@gmail.com>
2021-06-27 16:54:24 +0200

    Refactor

--- 4kyu-Balanced parentheses string-Patxi.py
+++ 4kyu-Balanced parentheses string-Patxi.py
@@ -15,11 +15,32 @@ def generateParentheses(openBr, closeBr, n, s=[]):
     return
 
 
-def balanced_parens(n, k):
+def balanced_parens1(n, k):
     global arr
     arr = []
     generateParentheses(0, 0, n)
     return arr[-(k+1)] if 0<=k<len(arr) else None
+
+
+def balanced_parens2(n, k):
+    def push_in(s, n1, n2):
+        if n1 + n2 == n * 2 and len(res)<=k+1  : res.append(s); return
+        if n1 < n and len(res)<=k+1            : push_in(s + '(', n1 + 1, n2)
+        if n1 - n2 > 0 and len(res)<=k+1       : push_in(s + ')', n1, n2 + 1)
+
+    res = []; push_in('', 0, 0)
+    return res[k] if 0<=k<len(res) else None
+
+
+def balanced_parens(n, k):
+    return list(set([p[:i] + "()" + p[i:] for p in balanced_parens(n - 1) for i in range(0, len(p))])) if n > 1 else ([""], ["()"])[n]
+
+r = balanced_parens(200, 10e6)
+
+
+
+
+
 
 
 
