d72d32a08bae6e7035a16bfc47695022e2054bff
Patxi Oyaga <oyaga888@gmail.com>
2021-06-27 16:58:00 +0200

    6kyu-Valid Braces-Patxi

--- 6kyu-Valid Braces-Patxi.py
+++ 6kyu-Valid Braces-Patxi.py
@@ -1,16 +1,6 @@
-def validBraces(string):
-    temp = []
-    for item in string:
-        if item in ['[','{','(']:
-            temp.insert(0,item)
-        if item in [']','}',')']:
-            if len(temp) == 0:
-                return False
-            elif (item == ']' and temp[0] == '[') or (item == '}' and temp[0] == '{') or (item == ')' and temp[0] == '('):
-                temp.remove(temp[0])
-            else:
-                return False
-    if len(temp) != 0:
-        return False
-    else:
-        return True
+def validBraces(s):
+  while '{}' in s or '()' in s or '[]' in s:
+      s=s.replace('{}','')
+      s=s.replace('[]','')
+      s=s.replace('()','')
+  return s==''
