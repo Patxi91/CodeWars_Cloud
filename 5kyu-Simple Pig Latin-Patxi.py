d884c1d86d315a3b421d6feaf75e69ece359dd1a
Patxi Oyaga <oyaga888@gmail.com>
2021-07-03 23:29:08 +0200

    Refactor

--- 5kyu-Simple Pig Latin-Patxi.py
+++ 5kyu-Simple Pig Latin-Patxi.py
@@ -1,13 +1,2 @@
 def pig_it(text):
-    words = text.split(' ')
-    pl_words = []
-    for w in words:
-        if w.isalpha():
-            if len(w) > 1:
-                pl_words.append(w[1:]+w[0]+'ay')
-            else:
-                pl_words.append(w + 'ay')
-        else:
-            pl_words.append(w)
-    pl_text = ' '.join(pl_words)
-    return pl_text
+    return " ".join(x[1:] + x[0] + "ay" if x.isalnum() else x for x in text.split())
