6c52ed4852e1257f7f26336c0b2ccd2a159c5d81
Patxi Oyaga <oyaga888@gmail.com>
2021-07-10 16:08:50 +0200

    4kyu-Sudoku Solution Validator-Patxi

--- 4kyu-Sudoku Solution Validator-Patxi.py
+++ 4kyu-Sudoku Solution Validator-Patxi.py
@@ -5,7 +5,7 @@ def valid_solution(board):
     nums = [i for i in range(1, 10)]
     bn = np.array(board)
     # Small Test 3x3
-    if sorted(list(itertools.chain.from_iterable(bn[:3, :3].tolist())))  != nums:
+    if sorted(list(itertools.chain.from_iterable(bn[:3, :3].tolist()))) != nums:
         return False
     # Rows
     for row in np.sort(bn):
