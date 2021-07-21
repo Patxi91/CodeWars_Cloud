4a0c78a09a54d8e9e4b8f3919a0dacbdc2781950
Patxi Oyaga <oyaga888@gmail.com>
2021-07-21 08:38:11 +0200

    6kyu-Buying a car-Patxi

---
+++ 6kyu-Buying a car-Patxi.py
@@ -0,0 +1,11 @@
+def nbMonths(old_car_price, new_car_price, saving_per_month, percent_loss_by_month):
+    savings = 0
+    month = 1
+    while old_car_price + savings < new_car_price:
+        if month % 2 == 0:
+            percent_loss_by_month += 0.5
+        month += 1
+        new_car_price -= new_car_price * percent_loss_by_month / 100
+        old_car_price -= old_car_price * percent_loss_by_month / 100
+        savings += saving_per_month
+    return [month - 1, round(savings + old_car_price - new_car_price)]
