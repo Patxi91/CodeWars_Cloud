d643f95a5a478ac6c0bfbd9d0f4e8d458e31a1d0
Patxi Oyaga <oyaga888@gmail.com>
2021-08-02 10:13:09 +0200

    5kyu-PaginationHelper-Patxi.py

---
+++ 5kyu-PaginationHelper-Patxi.py
@@ -0,0 +1,32 @@
+import math
+
+
+class PaginationHelper:
+
+    # The constructor takes in an array of items and a integer indicating
+    # how many items fit within a single page
+    def __init__(self, collection, items_per_page):
+        self.collection = collection
+        self.items_per_page = items_per_page
+
+    # returns the number of items within the entire collection
+    def item_count(self):
+        return len(self.collection)
+
+    # returns the number of pages
+    def page_count(self):
+        return math.ceil(len(self.collection) / self.items_per_page)
+
+    # returns the number of items on the current page. page_index is zero based
+    # this method should return -1 for page_index values that are out of range
+    def page_item_count(self, page_index):
+        if 0 <= page_index < self.page_count():
+            return self.items_per_page if page_index < self.page_count() - 1 else len(
+                self.collection) % self.items_per_page
+        else:
+            return -1
+
+    # determines what page an item is on. Zero based indexes.
+    # this method should return -1 for item_index values that are out of range
+    def page_index(self, item_index):
+        return math.ceil((item_index + 1) / self.items_per_page) - 1 if 0 <= item_index < len(self.collection) else -1
