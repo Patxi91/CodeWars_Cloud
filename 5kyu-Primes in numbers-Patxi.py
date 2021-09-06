fa77b3d38d13e0c81eb947348e22ddf935fb0d3a
Patxi Oyaga <oyaga888@gmail.com>
2021-09-06 22:42:47 +0200

    5kyu-Primes in numbers-Patxi.py

---
+++ 5kyu-Primes in numbers-Patxi.py
@@ -0,0 +1,27 @@
+def prime_factors(n):
+
+    i = 2
+    res = {}
+    while n/i != 1:
+        if n%i == 0:
+            if i in res:
+                res[i] = res[i]+1
+            else:
+                res[i] = 1
+            n = n/i
+        else:
+            i+=1
+
+    if i in res:
+        res[i] = res[i]+1
+    else:
+        res[i] = 1
+    t = ''
+    res = sorted(res.items(),key = lambda a:a[0])
+
+    for key in res:
+        if key[1] == 1:
+            t = t + '('+str(key[0]) +')'
+        else:
+            t = t + '(' +str(key[0]) + '**' + str(key[1]) + ')'
+    return t
