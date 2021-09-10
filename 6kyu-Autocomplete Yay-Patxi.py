51f8a3e524a4ac65aed56cbdb50b6b39a038a254
Patxi Oyaga <oyaga888@gmail.com>
2021-09-10 15:01:12 +0200

    6kyu-Autocomplete Yay-Patxi.py

---
+++ 6kyu-Autocomplete Yay-Patxi.py
@@ -0,0 +1,8 @@
+
+def only_az(s):
+    return ''.join(a for a in s if a.isalpha())
+
+
+def autocomplete(s, lst):
+    s = only_az(s)
+    return [a for a in lst if only_az(a).lower().startswith(s)][:5]
