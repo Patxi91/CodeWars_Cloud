a15d951a045498b20e946148b605d53a934a00a0
Patxi Oyaga <oyaga888@gmail.com>
2021-10-09 09:18:29 +0200

    6kyu-Matrix Expansion-Patxi.py

---
+++ 6kyu-Matrix Expansion-Patxi.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def expansion(matrix, n):
+    a = np.array(matrix)
+
+    while n > 0:
+        a = np.append(a, [np.sum(a, axis=0)], axis=0)  # Append row
+        col = np.array([np.sum(a, axis=1)])  # Calculate column
+        a = np.concatenate((a, col.T), axis=1)  # Concatenate column
+        a[-1][-1] = np.trace(a) - a[-1][-1]  # Correct last diagonal element
+
+    return a
