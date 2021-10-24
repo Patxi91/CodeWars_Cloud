2d00adb359345ed43ae9ba1a761e76a9040c589f
Patxi Oyaga <oyaga888@gmail.com>
2021-10-24 11:20:03 +0200

    5kyu-Write out numbers-Patxi.py

---
+++ 5kyu-Write out numbers-Patxi.py
@@ -0,0 +1,14 @@
+words = "zero one two three four five six seven eight nine" + \
+" ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty" + \
+" thirty forty fifty sixty seventy eighty ninety"
+words = words.split(" ")
+
+def number2words(n):
+    if n < 20:
+        return words[n]
+    elif n < 100:
+        return words[18 + n // 10] + ('' if n % 10 == 0 else '-' + words[n % 10])
+    elif n < 1000:
+        return number2words(n // 100) + " hundred" + (' ' + number2words(n % 100) if n % 100 > 0 else '')
+    elif n < 1000000:
+        return number2words(n // 1000) + " thousand" + (' ' + number2words(n % 1000) if n % 1000 > 0 else '')
