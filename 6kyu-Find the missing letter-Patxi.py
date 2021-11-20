a09ca1f457626dbbbb2d09fdca3eb65896af8810
Patxi Oyaga <oyaga888@gmail.com>
2021-11-20 18:19:46 +0100

    6kyu-Find the missing letter-Patxi.py

---
+++ 6kyu-Find the missing letter-Patxi.py
@@ -0,0 +1,7 @@
+import string
+
+def find_missing_letter(chars):
+    charset = string.ascii_lowercase if chars[0] >= 'a' else string.ascii_uppercase
+    for letter in charset[charset.index(chars[0]):]:
+        if letter not in chars:
+            return letter[0]
