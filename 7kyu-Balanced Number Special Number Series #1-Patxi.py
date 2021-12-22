109792e50b2d58ae77ed8b11fb708a16d14f5e46
Patxi Oyaga <oyaga888@gmail.com>
2021-12-22 09:02:13 +0100

    7kyu-Balanced Number Special Number Series #1-Patxi.py

---
+++ 7kyu-Balanced Number Special Number Series #1-Patxi.py
@@ -0,0 +1,5 @@
+def balanced_num(number):
+    s = str(number)
+    l = (len(s)-1)//2
+    same = len(s) < 3 or sum(map(int, s[:l])) == sum(map(int, s[-l:]))
+    return "Balanced" if same else "Not Balanced"
