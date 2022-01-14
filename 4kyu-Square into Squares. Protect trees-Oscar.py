900c3be4707160474c3ef08992086224bad18fd2
Oscar <oscar.oy@hotmail.com>
2022-01-14 13:47:40 +0100

    another answer

--- 4kyu-Square into Squares. Protect trees-Oscar.py
+++ 4kyu-Square into Squares. Protect trees-Oscar.py
@@ -1,15 +1,17 @@
 def decompose(n):
-    def _recurse(s, i):
-        if s < 0:
-            return None
-        if s == 0:
-            return []
-        for j in range(i-1, 0, -1):
-            sub = _recurse(s - j**2, j)
-            if sub != None:
-                return sub + [j]
-    return _recurse(n**2, n)
-
+    total = 0
+    answer = [n]
+    while len(answer):
+        print(answer)
+        temp = answer.pop()
+        total += temp ** 2
+        for i in range(temp - 1, 0, -1):
+            if total - (i ** 2) >= 0:
+                total -= i ** 2
+                answer.append(i)
+                if total == 0:
+                    return sorted(answer)
+    return None
 '''
 print(decompose(5))
 print(decompose(50))
