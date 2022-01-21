ac40e972fec3750a2c5b38e755d1c502ae6f160e
Oscar <oscar.oy@hotmail.com>
2022-01-21 12:06:09 +0100

    Keep going with the fibonacci

--- 3kyu-The Millionth Fibonacci Kata-Oscar.py
+++ 3kyu-The Millionth Fibonacci Kata-Oscar.py
@@ -1,12 +1,14 @@
 
-cache = {}
-def fib(n):
-    if n == 0 or n==1:
-        return n
+def fib(n, a = 0,b = 1):
+    print(n)
 
-    if n in cache:
-        return cache[n]
+    print(b)
+    if n!=0:
+        return fib(n-1,b,a+b)
     else:
-        cache[n] = fib(n-1)+fib(n-2)
+        return a+b
+    
+    
 
-    return cache[n]
+
+print(fib(1000))
