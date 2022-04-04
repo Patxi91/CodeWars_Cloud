5066477e1dbaaaf5ba3e00e079a6292b0567182c
Patxi Oyaga <oyaga888@gmail.com>
2022-04-04 23:11:46 +0200

    8kyu-Rock Paper Scissors-Patxi.py

---
+++ 8kyu-Rock Paper Scissors-Patxi.py
@@ -0,0 +1,21 @@
+def rps(p1, p2):
+    if p1 == p2:
+        return "Draw!"
+
+    if p1 == "scissors":
+        if p2 == "paper":
+            return "Player 1 won!"
+        elif p2 == "rock":
+            return "Player 2 won!"
+
+    if p1 == "paper":
+        if p2 == "rock":
+            return "Player 1 won!"
+        elif p2 == "scissors":
+            return "Player 2 won!"
+
+    if p1 == "rock":
+        if p2 == "scissors":
+            return "Player 1 won!"
+        elif p2 == "paper":
+            return "Player 2 won!"
