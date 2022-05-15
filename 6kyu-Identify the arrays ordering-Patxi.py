b7854cdc1a10c802b87d71077945b5a27667e815
Patxi Oyaga <oyaga888@gmail.com>
2022-05-15 16:52:57 +0200

    6kyu-Identify the arrays ordering-Patxi.py

---
+++ 6kyu-Identify the arrays ordering-Patxi.py
@@ -0,0 +1,28 @@
+def order_type(arr):
+    n = []
+
+    if len(arr) == 0:
+        return 'Constant'
+
+    for i in arr:
+        if type(i) == int:
+            i = str(i)
+        n.append(len(i))
+
+    if n[0] == n[-1]:
+        for i in n:
+            if i != n[0]:
+                return 'Unsorted'
+        return 'Constant'
+
+    elif n[0] < n[-1]:
+        for i, ele in enumerate(n[1:]):
+            if ele < n[i]:
+                return 'Unsorted'
+        return 'Increasing'
+
+    elif n[0] > n[-1]:
+        for i, ele in enumerate(n[1:]):
+            if ele > n[i]:
+                return 'Unsorted'
+        return 'Decreasing'
