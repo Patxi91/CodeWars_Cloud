6b29f5758e597d828ca359213f22892d1a7aa308
Patxi Oyaga <oyaga888@gmail.com>
2022-09-21 00:39:44 +0200

    7kyu-Count the divisors of a number-Patxi.py solution

--- 7kyu-Count the divisors of a number-Patxi.py
+++ 7kyu-Count the divisors of a number-Patxi.py
@@ -1,1 +1,17 @@
+import math
 
+
+# function to count the divisors
+def divisors(n):
+    cnt = 0
+    for i in range(1, (int)(math.sqrt(n)) + 1):
+        if (n % i == 0):
+
+            # If divisors are equal,
+            # count only one
+            if (n / i == i):
+                cnt = cnt + 1
+            else:  # Otherwise count both
+                cnt = cnt + 2
+
+    return cnt
