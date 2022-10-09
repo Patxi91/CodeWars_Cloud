5d6e0d44e91db9dba6f44a1160a63086908cc89e
Patxi Oyaga <oyaga888@gmail.com>
2022-10-09 18:33:57 +0200

    6kyu-Highest Rank Number in an Array-Patxi.py

---
+++ 6kyu-Highest Rank Number in an Array-Patxi.py
@@ -0,0 +1,13 @@
+def highest_rank(arr: list) -> int:
+    number_with_frequent: dict = {}
+    most_frequent_number: list = []
+    for i in arr:
+        if i not in number_with_frequent:
+            number_with_frequent[i] = 1
+        else:
+            number_with_frequent[i] += 1
+    for k, v in number_with_frequent.items():
+        max_value = max([v for v in number_with_frequent.values()])
+        if v == max_value:
+            most_frequent_number.append(k)
+    return sorted(most_frequent_number, reverse=True)[0]
