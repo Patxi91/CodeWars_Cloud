fa4feb40f932d4d62ec228f91d8d1f8e660a9cb8
Patxi Oyaga <oyaga888@gmail.com>
2022-10-16 22:47:54 +0200

    6kyu-Fold an array-Patxi.py

---
+++ 6kyu-Fold an array-Patxi.py
@@ -0,0 +1,17 @@
+import math
+
+
+def fold_array(array:list , runs:int) -> list:
+    for i in range(runs):
+        if len(array) == 1: return array
+        ret = []
+        start = 0
+        end = len(array)-1
+        while start != end and start < end:
+            ret.append(array[start] + array[end])
+            start += 1
+            end -= 1
+        if len(array) % 2:
+            ret.append(array[math.floor(len(array) / 2)])
+        array = ret
+    return array
