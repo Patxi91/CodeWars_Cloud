966f8ed153b84f05336307b8d4d7378e41b407b8
Patxi Oyaga <oyaga888@gmail.com>
2023-04-15 14:40:49 +0200

    passes performance tests - pending more efficient solution

--- 4kyu-Count Squares In the Chess Board-Patxi.py
+++ 4kyu-Count Squares In the Chess Board-Patxi.py
@@ -1,32 +1,29 @@
-def val_point(chessBoard, x, y, max, cache):
-    """Biggest possible square from a certain point."""
-    if (x, y, max) in cache:
-        return cache[(x, y, max)]
-    for i in range(max):
-        s = 0
-        width = max - i
-        for row in chessBoard[x:x+width]:
-            s += sum(row[y:y+width])
-            if s == width*width:
-                cache[(x, y, max)] = width
-                return width
-    cache[(x, y, max)] = None
-    return None
+import numpy as np
 
 def count(chessBoard):
-    dims = len(chessBoard)
-    points = list()
     r = dict()
     cache = {}
+    arr = np.array(chessBoard)
+    dims = arr.shape[0]
     for x in range(dims):
         xmax = dims - x
         for y in range(dims):
             max = min(xmax, dims - y)
-            biggest = val_point(chessBoard, x, y, max, cache)
-            if biggest is not None:
-                for i in range(2, biggest + 1):
-                    if i not in r:
-                        r[i] = 1
+            for i in range(2, max + 1):
+                if (x, y, i) in cache:
+                    biggest = cache[(x, y, i)]
+                else:
+                    sub_arr = arr[x:x+i, y:y+i]
+                    s = sub_arr.sum()
+                    if s == i*i:
+                        biggest = i
+                        cache[(x, y, i)] = i
                     else:
-                        r[i] += 1
+                        biggest = None
+                        cache[(x, y, i)] = None
+                if biggest is not None:
+                    if biggest not in r:
+                        r[biggest] = 1
+                    else:
+                        r[biggest] += 1
     return r
