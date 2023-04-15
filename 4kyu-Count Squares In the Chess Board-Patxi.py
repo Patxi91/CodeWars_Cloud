281f613998440d75eed79146115998078b9836ab
Patxi Oyaga <oyaga888@gmail.com>
2023-04-15 14:29:11 +0200

    improved efficiency - pending solution

--- 4kyu-Count Squares In the Chess Board-Patxi.py
+++ 4kyu-Count Squares In the Chess Board-Patxi.py
@@ -1,27 +1,29 @@
-def val_point(chessBoard, x, y, max):
+def val_point(chessBoard, x, y, max, cache):
     """Biggest possible square from a certain point."""
-    for i in range(max - 1):
+    if (x, y, max) in cache:
+        return cache[(x, y, max)]
+    for i in range(max):
         s = 0
-
         width = max - i
-        for row in chessBoard[x : x + width]:
-            s += sum(row[y : y + width])
+        for row in chessBoard[x:x+width]:
+            s += sum(row[y:y+width])
             if s == width*width:
+                cache[(x, y, max)] = width
                 return width
+    cache[(x, y, max)] = None
     return None
-
 
 def count(chessBoard):
     dims = len(chessBoard)
     points = list()
     r = dict()
-
-    for x in range(dims - 1):
+    cache = {}
+    for x in range(dims):
         xmax = dims - x
-        for y in range(dims - 1):
+        for y in range(dims):
             max = min(xmax, dims - y)
-            biggest = val_point(chessBoard, x, y, max)
-            if biggest != None:
+            biggest = val_point(chessBoard, x, y, max, cache)
+            if biggest is not None:
                 for i in range(2, biggest + 1):
                     if i not in r:
                         r[i] = 1
