1e710242e99f11bcd8431f39a37e5a038262af27
Patxi Oyaga <oyaga888@gmail.com>
2023-04-15 15:44:28 +0200

    fastest implementations - pending more efficient solution

--- 4kyu-Count Squares In the Chess Board-Patxi.py
+++ 4kyu-Count Squares In the Chess Board-Patxi.py
@@ -31,3 +31,19 @@ def count2(chessBoard):
     return r
 
 
+def count3(chessBoard):
+    r = {}
+    n = len(chessBoard)
+    arr = np.array(chessBoard, dtype=bool)
+    for k in range(2, n+1):
+        k_squared = k**2
+        sub_arr_shape = (n-k+1, n-k+1, k, k)
+        sub_arr_strides = arr.strides + arr.strides
+        sub_arr_view = np.lib.stride_tricks.as_strided(arr, shape=sub_arr_shape, strides=sub_arr_strides)
+        sub_arr_sum = np.sum(sub_arr_view, axis=(2, 3))
+        count = np.count_nonzero(sub_arr_sum == k_squared)
+        if count:
+            r[k] = count
+    return {k:v for k,v in r.items() if v}  # dictionary comprehension to remove keys with zero value
+
+
