74ed0de2ff5e580de413f8a9bbbea88296f883bc
Patxi Oyaga <oyaga888@gmail.com>
2023-08-04 22:47:25 +0200

    6kyu-Random Substitution Cipher-Patxi.py

--- 6kyu-Random Substitution Cipher-Patxi.py
+++ 6kyu-Random Substitution Cipher-Patxi.py
@@ -7,3 +7,4 @@ def random_sub():
     random.shuffle(shuffled_alphabet)
     substitution_cipher = {original: substituted for original, substituted in zip(alphabet, shuffled_alphabet)}
     return substitution_cipher
+
