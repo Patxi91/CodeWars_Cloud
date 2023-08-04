254d410c8a85b316b93a3a6e84c501b29bf00a1f
Patxi Oyaga <oyaga888@gmail.com>
2023-08-04 22:47:06 +0200

    6kyu-

---
+++ 6kyu-Random Substitution Cipher-Patxi.py
@@ -0,0 +1,9 @@
+import random
+
+
+def random_sub():
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shuffled_alphabet = list(alphabet)
+    random.shuffle(shuffled_alphabet)
+    substitution_cipher = {original: substituted for original, substituted in zip(alphabet, shuffled_alphabet)}
+    return substitution_cipher
