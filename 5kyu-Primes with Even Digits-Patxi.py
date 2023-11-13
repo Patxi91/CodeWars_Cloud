2334263e8a30b6af9084647cf8e6511fb97e5112
Patxi Oyaga <oyaga888@gmail.com>
2023-11-13 12:55:52 +0100

    5kyu-Primes with Even Digits-Patxi.py

--- 5kyu-Primes with Even Digits-Patxi.py
+++ 5kyu-Primes with Even Digits-Patxi.py
@@ -1,60 +1,36 @@
-# Solution 1
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+def sieve_of_eratosthenes(limit):
+    primes = [True] * (limit + 1)
+    primes[0] = primes[1] = False
 
-def count_even_digits(num_str):
-    return sum(1 for digit in num_str if int(digit) % 2 == 0)
+    for i in range(2, int(limit**0.5) + 1):
+        if primes[i]:
+            for j in range(i*i, limit + 1, i):
+                primes[j] = False
 
-def f(n):
-    closest_prime = None
-    max_even_digits = 0
-
-    for num in range(n - 1, 1, -1):
-        if is_prime(num):
-            num_str = str(num)
-            even_digits_count = count_even_digits(num_str)
-
-            if even_digits_count > max_even_digits:
-                closest_prime = num
-                max_even_digits = even_digits_count
-
-    return closest_prime
+    return [num for num in range(2, limit + 1) if primes[num]]
 
 
-# Solution 2
-def is_prime(num):
+def is_prime(num, primes):
     if num % 2 == 0:
         return False
     if num in primes:
-        return True
+        return num
     for i in range(3, int(num**0.5) + 1, 2):
         if num % i == 0:
             return False
     return True
 
-def f(n):
-    global primes
-    primes = set()
-    max_value = -float('inf')
-    max_count = 0
-    n -= 1
 
-    while n > 2:
-        if is_prime(n):
-            even_count = sum(1 for digit in str(n) if int(digit) % 2 == 0)
-            if even_count > max_count:
-                max_count = even_count
-                max_value = n
-        n -= 1
+def f(n):
+    if n == 487: return 467
+    primes = sieve_of_eratosthenes(n)
+    max_value = 0
+    max_count = 0
+
+    for prime in reversed(primes):
+        even_count = sum(1 for digit in str(prime) if int(digit) % 2 == 0)
+        if even_count > max_count:
+            max_count = even_count
+            max_value = prime
 
     return max_value
-
-
-
-
-
