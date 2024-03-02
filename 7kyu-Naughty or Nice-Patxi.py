8b6537bfb8f4a0a6c3966fc59b64dff66ad7428a
Patxi Oyaga <oyaga888@gmail.com>
2024-03-02 23:54:37 +0100

    7kyu-Naughty or Nice-Patxi.py

--- 7kyu-Naughty or Nice-Patxi.py
+++ 7kyu-Naughty or Nice-Patxi.py
@@ -1,2 +1,19 @@
 def naughty_or_nice(data):
-    pass
+    naughty_count = 0
+    nice_count = 0
+
+    # Iterate through each month
+    for month, days in data.items():
+        # Iterate through each day in the month
+        for day, behavior in days.items():
+            if behavior == 'Naughty':
+                naughty_count += 1
+            elif behavior == 'Nice':
+                nice_count += 1
+
+    if naughty_count > nice_count:
+        return "Naughty!"
+    elif nice_count > naughty_count:
+        return "Nice!"
+    else:
+        return "Nice!"
